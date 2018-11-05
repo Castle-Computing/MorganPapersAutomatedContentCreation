b876602106986953bc418235f037e8c2398ee208
Luca Soares <luca_mps@hotmail.com>
2018-11-04 17:13:50 -0800

    Added Doc and basic TF-IDF analysis.

--- GetNounsNLP/NLP.py
+++ GetNounsNLP/NLP.py
@@ -1,9 +1,24 @@
+import os
 import nltk
+from nltk.text import TextCollection
 
 SEPARATORS = ['.', ',', ':', ';', '?', '!']
 NOUNS_TAGS = ['NN', 'NNP', 'NNS', 'NNPS']
 
 def getWords(text):
+    """
+    Gets a 2D list of the words in a text string
+
+    Args:
+        text: a string to be parsed
+
+    Returns:
+        a list of the of the following structure:
+        "Hello World! Goodbye Cruel World!"
+        becomes:
+        [["Hello", "World", "!"], ["Goodbye", "Cruel", "World", "!"]]
+    """
+
     sentances = nltk.sent_tokenize(text)
     phrases = []
 
@@ -13,6 +28,16 @@ def getWords(text):
     return phrases
 
 def getNoums(phrases):
+    """
+    gets a list of nouns from a 2D list created from getWords()
+
+    Args:
+        phrases: a 2D list to be analyzed
+
+    Returns:
+        a list containing all the nouns
+    """
+
     nouns = []
 
     for words in phrases:
@@ -44,6 +69,16 @@ def getNoums(phrases):
     return nouns
 
 def getStems(phrases):
+    """
+    gets a list of stems (roots) of words from a 2D list created from getWords()
+
+    Args:
+        phrases: a 2D list to be analyzed
+
+    Returns:
+        a list containing all the stems (roots)
+    """
+
     stemmer = nltk.stem.porter.PorterStemmer()
     stems = []
     for sentance in phrases:
@@ -53,13 +88,38 @@ def getStems(phrases):
 
     return stems
 
+def getTF(words, textCol, text):
+    wordFreq = []
+    for word in words:
+        wordFreq.append(textCol.tf(word, text))
+
+    return wordFreq
+
+def getIDF(words, textCol):
+    wordIDF = []
+    for word in words:
+        wordIDF.append(textCol.idf(word))
+
+    return wordIDF
+
 def main():
-    textFile = open('randomLetter.txt', 'r')
-    text = textFile.read()
-    phrases = getWords(text)
+    letters = os.listdir('./ocrList')
+    texts = []
+    for letter in letters:
+        texts.append(open('ocrList/' + letter, 'r'))
+
+    textsStings = []
+    for text in texts:
+        textsStings.append(text.read())
+
+    collection = TextCollection(textsStings)
+    phrases = getWords(textsStings[0])
     print phrases
     print getStems(phrases)
-    print getNoums(phrases)
+    nouns = getNoums(phrases)
+    print nouns
+    print getTF(nouns, collection, textsStings[0])
+    print getIDF(nouns, collection)
     return 0
 
 if __name__ == '__main__':
